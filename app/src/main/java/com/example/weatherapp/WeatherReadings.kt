1b2905bfce72cae6da02269174245c490bbc895a
benjamingandeza <50702379+benjamingandeza@users.noreply.github.com>
2019-05-17 12:06:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/WeatherReadings.kt
@@ -0,0 +1,13 @@
+package com.example.weatherapp
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherReadings : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.readings_weather)
+    }
+}
