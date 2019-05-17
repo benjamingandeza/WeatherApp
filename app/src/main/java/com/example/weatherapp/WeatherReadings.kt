3dae48850c96879a45d621d15e9b82f5a434cb1d
benjamingandeza <50702379+benjamingandeza@users.noreply.github.com>
2019-05-17 17:57:51 +0800

    XML and Functions in the activity

--- app/src/main/java/com/example/weatherapp/WeatherReadings.kt
+++ app/src/main/java/com/example/weatherapp/WeatherReadings.kt
@@ -3,11 +3,27 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Typeface
+import android.widget.ProgressBar
+import android.widget.TextView
+
+
 
 class WeatherReadings : AppCompatActivity() {
 
 
-    var city = "Dhaka, BD"
+    var selectCity: TextView? = null
+    var cityField:TextView? = null
+    var detailsField:TextView? = null
+    var currentTemperatureField:TextView? = null
+    var humidity_field:TextView? = null
+    var pressure_field:TextView? = null
+    var weatherIcon:TextView? = null
+    var updatedField:TextView? = null
+    var loader: ProgressBar? = null
+    var weatherFont: Typeface? = null
+
+    var OPEN_WEATHER_MAP_API = "9b6ff88cbef9de15b70cf2084a747ac0"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
