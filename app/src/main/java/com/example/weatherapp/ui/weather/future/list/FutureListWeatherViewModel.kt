9f0630d04808b2667d578f4d590bc1e2d4c986b2
benjamingandeza <50702379+benjamingandeza@users.noreply.github.com>
2019-05-20 00:45:17 +0800

    Major Changes in Functionalities, Design and API

---
+++ app/src/main/java/com/example/weatherapp/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.weatherapp.ui.weather.future.list
+
+import com.example.weatherapp.data.provider.UnitProvider
+import com.example.weatherapp.data.repository.ForecastRepository
+import com.example.weatherapp.internal.lazyDeferred
+import com.example.weatherapp.ui.base.WeatherViewModel
+import org.threeten.bp.LocalDate
+
+class FutureListWeatherViewModel(
+    private val forecastRepository: ForecastRepository,
+    unitProvider: UnitProvider
+) : WeatherViewModel(forecastRepository, unitProvider) {
+
+    val weatherEntries by lazyDeferred {
+        forecastRepository.getFutureWeatherList(LocalDate.now(), super.isMetricUnit)
+    }
+}
