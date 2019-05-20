5860b1a5699279d632e836a8c1c1d48190992c69
benjamingandeza <50702379+benjamingandeza@users.noreply.github.com>
2019-05-20 16:11:44 +0800

    Update API

--- app/src/main/java/com/example/weatherapp/data/network/ApixuWeatherApiService.kt
+++ app/src/main/java/com/example/weatherapp/data/network/ApixuWeatherApiService.kt
@@ -13,7 +13,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 
-const val API_KEY = "89e8bd89085b41b7a4b142029180210"
+const val API_KEY = "22b571c7b0874b71b4d80927192005"
 
 //http://api.apixu.com/v1/current.json?key=89e8bd89085b41b7a4b142029180210&q=London&lang=en
 
